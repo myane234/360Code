10b58abcec889ab8a16577a8844da218110efde5
myane <157201609+myane234@users.noreply.github.com>
2025-10-29 23:02:36 +0700

    fix bug n add append n pow, modulus

--- calculator/hitung.js
+++ calculator/hitung.js
@@ -1,18 +1,26 @@
+module.exports = { plus, minus, multiply, devide, modulus, pow };
 
-module.exports = { plus, minus, multiply, devide };
-
-function plus(a,b) {
-    return a + b;
+function plus(a, b) {
+  return a + b;
 }
 
-function minus(a,b) {
-    return a - b;
+function minus(a, b) {
+  return a - b;
 }
 
-function multiply(a,b) {
-    return a * b;
+function multiply(a, b) {
+  return a * b;
 }
 
-function devide(a,b) {
-    return a / b;
+function devide(a, b) {
+  return a / b;
+}
+
+function modulus(a, b) {
+  return a % b;
+}
+
+// pangkat
+function pow(a, b) {
+  return Math.pow(a, b);
 }
--- calculator/index.js
+++ calculator/index.js
@@ -1,60 +1,76 @@
-const readline = require('readline');
-const { plus, minus, multiply, devide } = require('./hitung.js');
-const fs = require('fs');
+const readline = require("readline");
+const { plus, minus, multiply, devide, modulus, pow } = require("./hitung.js");
+const fs = require("fs");
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
-
 function pertanyaan() {
-rl.question('Enter ur first Number', (a) => {
-    const firstNumber = parseFloat(a);
+  rl.question("Enter ur first Number: ", (firstNumber) => {
+    const a = parseFloat(firstNumber);
 
-    rl.question('Enter ur second Number', (b) => {
-        const secondNumber = parseFloat(b);
+    rl.question("Enter ur second Number: ", (secondNumber) => {
+      const b = parseFloat(secondNumber);
 
-    rl.question(`r u want?, plus, minus, multiply, devide `, (operation) => {
-
-        let result;
-        if (operation === 'plus')  {
-            result = plus(a,b);
-        }
-        else if(operation === 'minus') {
-            result = minus(a,b);
-            console.log(result)
-        }
-
-        else if(operation === 'multiply') {
-            result = multiply(a,b);
-        }
-        else if(operation === 'devide') {
-          result = devide(a,b);
-        } else {
+      rl.question(
+        `r u want?, 
+        +,-,X,/,%,pow(pangkat) : `,
+        (operation) => {
+          let result;
+          if (operation === "+") {
+            result = plus(a, b);
+          } else if (operation === "-") {
+            result = minus(a, b);
+            console.log(result);
+          } else if (operation === "X") {
+            result = multiply(a, b);
+          } else if (operation === "/") {
+            result = devide(a, b);
+          } else if (operation === "%") {
+            result = modulus(a, b);
+          } else if (operation === "pangkat" || operation === "pow") {
+            result = pow(a, b);
+          } else {
             console.log(`Masukkin yang bener`);
             pertanyaan();
+          }
+          console.log(`Hasil dari ${a} ${operation} ${b}: ${result}`);
+          // ubah data menjadi string if u want to write to file
+          //   fs.writeFileSync("data.txt", result.toString(), "utf-8", (err) => {
+          //     if (err) throw err;
+          //     console.log("Berhasil tulis");
+          //   });
+
+          // using this method if u want to nambah data tanpa ubah data lama, belajar cara promise nya juga ya
+          fs.appendFile(
+            "data.txt",
+            `Hasil dari ${a} ${operation} ${b}: ` + result.toString() + "\n",
+            "utf-8",
+            (err) => {
+              if (err) throw err;
+              console.log(
+                `Berhasil menambahkan ${a} ${operation} ${b}: ${result} ke data.txt`
+              );
+            }
+          );
+          loop();
         }
-        console.log(`Hasil dari ${a} ${operation} ${b}: ${result}`);
-        fs.writeFile('data.txt', result, 'utf-8', (err) => {
-            if (err) throw err;
-            console.log('Berhasil tulis');
-        })
-        loop();
-    })
-    })
-})
+      );
+    });
+  });
 }
 pertanyaan();
 
 function loop() {
-    console.log('anda masuk ke loop')
-    rl.question('Mau lanjut?(y/n)', (answer) => {
-        if (answer.toLowerCase() === 'y') {
-            pertanyaan();
-        } else if(answer.toLowerCase() === 'n') {
-            console.log(`Terimakasih sudah menggunakan kalkulator`)
-            rl.close();
-        }
-    })
+  console.log("anda masuk ke loop");
+  rl.question("Mau lanjut?(y/n): ", (answer) => {
+    if (answer.toLowerCase() === "y") {
+      pertanyaan();
+    } else if (answer.toLowerCase() === "n") {
+      console.log(`Terimakasih sudah menggunakan kalkulator`);
+      rl.close();
+    }
+  });
 }
